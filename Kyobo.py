1da82727556dc3501f8b5cc1eaeda96f456fed88
zae-park <zae_park@huinno.com>
2022-11-22 00:07:40 +0900

    add review score crawler

--- Kyobo.py
+++ Kyobo.py
@@ -2,12 +2,13 @@ import os
 import time
 import shutil
 import glob
-from bs4 import BeautifulSoup
+import csv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 import chromedriver_autoinstaller
 
@@ -49,67 +50,6 @@ class Kyobo:
     driver.implicitly_wait(10)
 
   def download_csv(self):
-    # Top categories
-    driver = webdriver.Chrome(f'./{self.chrome_ver}/chromedriver.exe', options=self.sele_options)
-    driver.get(self.domain)
-
-    detail_dict = {}
-    if not driver.get_issue_message():
-      fold_menu = '#welcome_header_wrap > div.header_inner > nav > div.anb_wrap > button'
-      fold_menu = driver.find_element(By.CSS_SELECTOR, fold_menu)
-      fold_menu.send_keys(Keys.ENTER)
-
-      mainpages = '#tabAnbCategoryKyobo > div.anb_category_inner > div > div.tab_list_wrap > ul > li'
-      mainpages = driver.find_elements(By.CLASS_NAME, 'tab_link')
-      # mainpage_urls = {mp.text.replace('/', '+'): mp.get_attribute('href') for mp in driver.find_elements(By.CLASS_NAME, 'tab_link ui-tabs-anchor')}
-
-      for mainpage in mainpages:
-        time.sleep(1) # wait
-        main_name = mainpage.text
-        mainpage.send_keys(Keys.ENTER)
-
-        fold_btn = '#tabAnbCategorySub01 > div.custom_scroll_wrap > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div > div.category_view_area > div > div:nth-child(1) > ul > li:nth-child(1) > div.fold_box_header > button'
-        fold_btn = driver.find_element(By.CSS_SELECTOR, fold_btn)
-        fold_btn.send_keys(Keys.ENTER)
-        time.sleep(0.5) # wait
-      
-        subpages = '#tabAnbCategorySub01 > div.custom_scroll_wrap.active > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div > div.category_view_area > div > div:nth-child(1) > ul > li.fold_box.expanded > div.fold_box_contents > ul > li'
-        subpages = driver.find_elements(By.CSS_SELECTOR, subpages)
-        subpage_urls = {sub.text.replace('/', '+'): sub.get_attribute('href') for sub in driver.find_elements(By.CLASS_NAME, 'category_link')}
-        
-        for subpage_name, subpage_url in subpage_urls.items():
-          time.sleep(1) # wait
-          # subpage_url = subpage.find_element(By.TAG_NAME, 'a').get_attribute('href')
-          driver.get(subpage_url)
-          time.sleep(3) # wait
-
-          details = '#contents > div > aside > div.aside_body > div.snb_wrap > ul > li'
-          details = driver.find_elements(By.CSS_SELECTOR, details)
-          detail_urls = {u.text.replace('/', '+'): u.get_attribute('href') for u in driver.find_elements(By.CLASS_NAME, 'snb_link')}
-
-          for detail_name, detail_url in detail_urls.items():
-            driver.get(detail_url)
-            time.sleep(3) # wait
-
-            selector = '#homeTabAll > div.list_result_wrap > div.right_area > div:nth-child(2) > button'
-            element = driver.find_element(By.CSS_SELECTOR, selector)
-            
-            print(f'\tCrawl... {main_name} - {subpage_name} - {detail_name}')
-
-            excel_btn = '#homeTabAll > div.list_result_wrap > div.right_area > div:nth-child(2) > button'
-            excel_btn = driver.find_element(By.CSS_SELECTOR, excel_btn)
-            excel_btn.click()
-            time.sleep(3)  # Waiting for download.
-            
-            downed = glob.glob(os.path.join(self.defualt_download, '상품목록*.xlsx'))
-            if downed:
-              _, extension = os.path.splitext(downed[0])
-              shutil.copy(downed[0], os.path.join(self.download_path, f'{main_name}_{subpage_name}_{detail_name}' + extension))
-              os.remove(downed[0])
-    else:
-      raise AssertionError
-
-  def download_csv2(self):
     # Top categories
     driver = webdriver.Chrome(f'./{self.chrome_ver}/chromedriver.exe', options=self.sele_options)
     
@@ -193,18 +133,173 @@ class Kyobo:
             time.sleep(3)  # Waiting for download.
             self.replace_xlsx(dst=f'{sup_name}_{subpage_name}_{tab_name}')
   
-  def click_btn(self, button):
-    excel_btn = self.driver.find_element(By.CSS_SELECTOR, excel_btn)
+  def download_csv3(self, DEBUG_mode=False):
+    # Go to domain
+    self.driver.get(self.domain)
+    time.sleep(2) # wait
+    
+    # Click main menu fold
+    main_menu = '#welcome_header_wrap > div.header_inner > nav > div.anb_wrap > button'
+    main_menu = self.driver.find_element(By.CSS_SELECTOR, main_menu)
+    main_menu.send_keys(Keys.ENTER)
+    time.sleep(2) # wait
+    
+    # Loop - Sup categories
+    for i_sup, sup_name in enumerate(['국내도서', '서양도서', '일본도서']):
+      # Click sup category tab
+      supcategories = self.driver.find_element(By.ID, 'tabAnbCategoryKyobo')
+      supcategories = supcategories.find_elements(By.CLASS_NAME, 'tab_item')
+      supcategory = None
+      for sup in supcategories:
+        if sup.text == sup_name:
+          supcategory = sup
+          break
+      supcategory.send_keys(Keys.ENTER)
+      time.sleep(2) # wait
+      
+      # Search sub category
+      sub_area = f'#tabAnbCategorySub{i_sup+1:02d} > div.custom_scroll_wrap > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div > div.category_view_area'
+      sub_area = self.driver.find_element(By.CSS_SELECTOR, sub_area)
+      sub_fold = sub_area.find_elements(By.CLASS_NAME, 'fold_box')  # Fold buttons for every sub-categories.
+      
+      # Search [Sub & Detail] pages.
+      # Loop - Sub categories
+      sub_detail_dict = {}
+      for area in sub_fold:
+        # Unfold sub-category to find detail categories.
+        btn = area.find_element(By.CLASS_NAME, 'btn_fold')
+        btn.send_keys(Keys.ENTER)
+        time.sleep(2) # wait
+        
+        sub_folded = self.driver.find_element(By.CLASS_NAME, 'fold_box.expanded')
+        subpage_name = sub_folded.find_element(By.CLASS_NAME, 'fold_box_header').text.replace('/', '+') # replace '/' to '+' to avoid conflict.
+        details = sub_folded.find_elements(By.CLASS_NAME, 'category_item')
+        
+        # Loop - detail categories
+        for detail in details:
+          detail_name = detail.text.replace('/', '+') # replace '/' to '+' to avoid conflict.
+          sub_detail_dict[f"{subpage_name}_{detail_name}"] = detail.find_element(By.TAG_NAME, 'a').get_attribute('href')   # [sub_detail] : [url]
+        
+        if DEBUG_mode: break
+      
+      # Loop - Detail categories
+      for sub_detail_name, page_url in sub_detail_dict.items():
+        self.driver.get(page_url)
+        time.sleep(3) # wait
 
+        # Search tailend categories - sometime it is not exist.
+        tailends = self.driver.find_elements(By.CLASS_NAME, 'snb_link')
+        tailend_dict = {tail.text.replace('/', '+'): tail.get_attribute('href') for tail in tailends}
+        
+        # Loop - tailend
+        csv_list = []
+        if tailend_dict:
+          for tailend_name, tailend_url in tailend_dict.items():
+            print(f'\tCrawl... {sup_name} - {subpage_name} - {sub_detail_name} - {tailend_name}')
+            save_name = f'{sup_name}_{subpage_name}_{sub_detail_name}_{tailend_name}'
+            
+            self.driver.get(tailend_url)
+            time.sleep(3) # wait
+            
+            # Click review tab
+            review_tab = self.driver.find_element(By.CSS_SELECTOR, '#ui-id-44')
+            review_tab.click()
+            time.sleep(2)
+            
+            # Get # of pages & get all book info.
+            try:
+              num_page = int(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > div > a[data-role="last"]').text)
+            except NoSuchElementException:
+              time.sleep(2)
+              num_page = int(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > div > a[data-role="last"]').text)
+            
+            # Search all books with reviews
+            books = {}
+            for i in range(num_page-1):
+              if i != 0:
+                self.click_btn(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > button.btn_page.next'))
+              
+              try:
+                book_lst = self.driver.find_elements(By.CSS_SELECTOR, '#homeTabReview > div.switch_prod_wrap.view_type_list > ol > li')
+              except NoSuchElementException:
+                time.sleep(2)
+                book_lst = self.driver.find_elements(By.CSS_SELECTOR, '#homeTabReview > div.switch_prod_wrap.view_type_list > ol > li')
+                
+              for book_ele in book_lst:
+                book_info = book_ele.find_element(By.CLASS_NAME, 'prod_info')
+                books[book_info.text] = book_info.get_attribute('href')
+              
+              if DEBUG_mode: break
+            
+            # Loop - books
+            for book_name, book_url in books.items():
+              self.driver.get(book_url)
+              self.click_btn(self.driver.find_element(By.CLASS_NAME, 'btn_go_review'))
+              score = self.driver.find_element(By.CSS_SELECTOR, '#ReviewList1 > div.klover_review_box > div.klover_box_left > div.box_top > div > div.caption > span > span.val')
+              score = float(score.text)
+              
+              csv_list.append([book_name, score]) # append book - score
+              if DEBUG_mode: break
+            if DEBUG_mode: break
+        else:
+          print(f'\tCrawl... {sup_name} - {subpage_name} - {sub_detail_name}')
+          save_name = f'{sup_name}_{subpage_name}_{sub_detail_name}'
+          
+          # Click review tab
+          review_tab = self.driver.find_element(By.CSS_SELECTOR, '#ui-id-44')
+          review_tab.click()
+          time.sleep(2)
+          
+          # Get # of pages & get all book info.
+          try:
+            num_page = int(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > div > a[data-role="last"]').text)
+          except NoSuchElementException:
+            time.sleep(2)
+            num_page = int(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > div > a[data-role="last"]').text)
+          
+          # Search all books with reviews
+          books = {}
+          for i in range(num_page-1):
+            if i != 0:
+              self.click_btn(self.driver.find_element(By.CSS_SELECTOR, '#reviewTopPagi > button.btn_page.next'))
+            
+            try:
+              book_lst = self.driver.find_elements(By.CSS_SELECTOR, '#homeTabReview > div.switch_prod_wrap.view_type_list > ol > li')
+            except NoSuchElementException:
+              time.sleep(2)
+              book_lst = self.driver.find_elements(By.CSS_SELECTOR, '#homeTabReview > div.switch_prod_wrap.view_type_list > ol > li')
+              
+            for book_ele in book_lst:
+              book_info = book_ele.find_element(By.CLASS_NAME, 'prod_info')
+              books[book_info.text] = book_info.get_attribute('href')
+            
+            if DEBUG_mode: break
+          
+          # Loop - books
+          for book_name, book_url in books.items():
+            self.driver.get(book_url)
+            self.click_btn(self.driver.find_element(By.CLASS_NAME, 'btn_go_review'))
+            score = self.driver.find_element(By.CSS_SELECTOR, '#ReviewList1 > div.klover_review_box > div.klover_box_left > div.box_top > div > div.caption > span > span.val')
+            score = float(score.text)
+            
+            csv_list.append([book_name, score]) # append book - score
+            if DEBUG_mode: break
+        
+        # Save csv
+        with open(os.path.join(self.download_path, f'{save_name}.csv'), 'w',newline='\n') as f:
+            writer = csv.writer(f)
+            writer.writerow(csv_list)
+    
+  def click_btn(self, button):
     try:
-      excel_btn.click()
+      button.click()
     except:
       try:
         time.sleep(2)
-        excel_btn.click()
+        button.click()
       except:
           pass
-    time.sleep(5)  # Waiting for download.
+    time.sleep(4)  # Waiting for download.
 
   def replace_xlsx(self, dst):
     downed = glob.glob(os.path.join(self.defualt_download, '상품목록*.xlsx'))
@@ -221,13 +316,18 @@ class Kyobo:
 
     self.download_csv2()
   
-  @classmethod
-  def get_reviews():
-    # TODO: 동작구 도서관에서 대여자 - 도서 목록을 응답할 경우.
+  def get_reviews(self, *args):
+    # Initial setting.
+    self.set_selenium_option()
+    self.check_chrome_version()
+    self.driver = webdriver.Chrome(f'./{self.chrome_ver}/chromedriver.exe', options=self.sele_options)
+
+    self.download_csv3(*args)
     return 2
 
 
 if __name__ == '__main__':
+  Kyobo().get_reviews(True)
   Kyobo().get_books()
   print('done')
   
